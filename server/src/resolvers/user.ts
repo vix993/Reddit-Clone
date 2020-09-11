139c702f709bfbe1b201dc0889aa94904caf605f
Victor <victor.as.nascimento@gmail.com>
2020-09-11 19:28:04 -0300

    adding query to db

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 import { MyContext } from "src/types";
 import { User } from "../entities/User";
 import argon2 from 'argon2';
+import { EntityManager } from '@mikro-orm/postgresql';
 
 // creating an input object
 
@@ -78,15 +79,18 @@ export class UserResolver {
                 ]
             }
         }
-        const hashedPassword = await argon2.hash(options.password)
-        const user = em.create(User, {
-            username: options.username,
-            password: hashedPassword
-        })
+        const hashedPassword = await argon2.hash(options.password);
+        let user;
         try {
-            await em.persistAndFlush(user)
+            const result = await (em as EntityManager).createQueryBuilder(User).getKnexQuery().insert({
+                username: options.username,
+                password: hashedPassword,
+                created_at: new Date(),
+                updated_at: new Date()
+            })
+            .returning("*");
+            user = result[0]
         } catch (error) {
-            // || error.detail.includes("already exists")
             // duplicate username error
             if (error.code === "23505"){
                 return {
